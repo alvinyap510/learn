381b6480a10d54420363655bbd2f19a5f0ae8222
alvinyap510 <leaf.alvin@gmail.com>
2022-03-19 18:17:47 +0800

    freeCodeCamp

---
+++ Programiz - C/C - Flow Control/switch_statement.c
@@ -0,0 +1,55 @@
+// Create a programme that will print the month based on user's input
+
+#include <stdio.h>
+
+int main (void)
+{
+    int number;
+
+    printf("Please input a number between 1 and 12: ");
+    scanf("%i", &number);
+
+    switch(number)
+    {
+        case 1:
+            printf("January\n");
+            break;
+        case 2:
+            printf("February\n");
+            break;
+        case 3:
+            printf("March\n");
+            break;
+        case 4:
+            printf("April\n");
+            break;
+        case 5:
+            printf("May\n");
+            break;
+        case 6:
+            printf("June\n");
+            break;
+        case 7:
+            printf("July\n");
+            break;
+        case 8:
+            printf("August\n");
+            break;
+        case 9:
+            printf("September\n");
+            break;
+        case 10:
+            printf("October\n");
+            break;
+        case 11:
+            printf("November\n");
+            break;
+        case 12:
+            printf("December\n");
+            break;
+        default:
+            printf("Invalid number\n");
+    }
+    return (0);
+}
+
