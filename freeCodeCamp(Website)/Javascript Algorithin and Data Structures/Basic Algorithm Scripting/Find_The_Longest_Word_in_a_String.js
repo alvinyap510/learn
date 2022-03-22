a90b5a0bfbe2235bc592f3d87c9047c8242f0dd2
alvinyap510 <leaf.alvin@gmail.com>
2022-03-23 02:28:51 +0800

    freeCodeCamp

---
+++ freeCodeCamp(Website)/Javascript Algorithin and Data Structures/Basic Algorithm Scripting/Find_The_Longest_Word_in_a_String.js
@@ -0,0 +1,15 @@
+function findLongestWordLength(str) {
+    let regex = /\w+/g;
+    let str_arr = str.match(regex);
+  
+    let longest = 0;
+  
+    for (let i = 0; i < str_arr.length; i++) {
+      if (str_arr[i].length >= longest) {
+        longest = str_arr[i].length;
+      }
+    }
+    return longest;
+  }
+  
+  findLongestWordLength("The quick brown fox jumped over the lazy dog");
