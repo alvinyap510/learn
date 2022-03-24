5acb5692b63a72ed0fc894976d700dce93ea7e6f
alvinyap510 <leaf.alvin@gmail.com>
2022-03-24 13:13:06 +0800

    fCC

---
+++ freeCodeCamp(Website)/Javascript Algorithin and Data Structures/Intermediate Algorithm Scripting/Diff_Two_Arrays.js
@@ -0,0 +1,16 @@
+function diffArray(arr1, arr2) {
+    const newArr = [];
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr2.includes(arr1[i]) == false) {
+        newArr.push(arr1[i]);
+      }
+    }
+    for (let i = 0; i < arr2.length; i++) {
+      if (arr1.includes(arr2[i]) == false) {
+        newArr.push(arr2[i]);
+      }
+    }
+    return newArr;
+  }
+  
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
