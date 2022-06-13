d78d94b708345c18040a3fb96a2c20af38dfe444
alvinyap510 <leaf.alvin@gmail.com>
2022-06-13 08:46:57 +0800

    key encryption

--- fCC(YT) - Solidity/hh-fcc/ethers-simple-storage-fcc/deploy.js
+++ fCC(YT) - Solidity/hh-fcc/ethers-simple-storage-fcc/deploy.js
@@ -1,10 +1,17 @@
 const ethers = require("ethers");
+const { readFileSync } = require("fs");
 const fs = require("fs-extra");
 require("dotenv").config();
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  // const encryptedJson = readFileSync("./.encryptedKey.json", "utf8");
+  // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+  //   encryptedJson,
+  //   process.env.PRIVATE_KEY_PASSWORD
+  // );
+  // wallet = await wallet.connect(provider);
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
